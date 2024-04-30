ad533c79d445334c910984d38eac5a3b6fccbb6f
Chris Zaharia <chris.zaharia@gmail.com>
2024-04-30 23:47:55 +1000

    Rename thalamus-like function and add docstrings

--- aiden/api/brain.py
+++ aiden/api/brain.py
@@ -18,13 +18,24 @@ logger = logging.getLogger("uvicorn")
 COGNITIVE_API_URL = f'{os.environ.get("COGNITIVE_API_PROTOCOL", "http")}://{os.environ.get("COGNITIVE_API_HOST", "localhost")}:{os.environ.get("COGNITIVE_API_PORT", "8000")}/api/chat'
 
 
-async def rewrite_user_prompt(initial_prompt: str):
+async def thalamus(integrated_sensory_data: str) -> str:
+    """
+    Simulate the thalamic process of rewriting the received prompt to ensure it
+    matches a specific narrative structure. It requests a rewriting from the
+    Cognitive API and handles the response.
+
+    Args:
+        integrated_sensory_data (str): The initial sensory data and narrative content.
+
+    Returns:
+        str: The rewritten prompt or the original if the request fails.
+    """
     messages = [
         Message(
             role="system",
             content="Rewrite the prompt you receive from a first-person perspective.",
         ),
-        Message(role="user", content=initial_prompt),
+        Message(role="user", content=integrated_sensory_data),
     ]
 
     chat_message = ChatMessage(
@@ -38,15 +49,31 @@ async def rewrite_user_prompt(initial_prompt: str):
         if response.status_code == 200:
             logger.info(f"Rewritten response json: {response.json()}")
             rewritten_prompt = (
-                response.json().get("message", {}).get("content", initial_prompt)
+                response.json()
+                .get("message", {})
+                .get("content", integrated_sensory_data)
             )
             return rewritten_prompt
         else:
-            return initial_prompt
+            return integrated_sensory_data
 
 
 @app.post("/cortical/")
-async def cortical(request: CorticalRequest):
+async def cortical(request: CorticalRequest) -> StreamingResponse:
+    """
+    Handle requests to process sensory data through the cognitive API to simulate
+    brain cortical activity. It constructs the narrative framework and sends it
+    for cognitive processing, streaming the response back to the client.
+
+    Args:
+        request (CorticalRequest): The incoming request with sensory data and configuration.
+
+    Returns:
+        StreamingResponse: The continuous response stream from the cognitive model.
+
+    Raises:
+        HTTPException: Raises an exception for any internal processing errors.
+    """
     logger.info(f"Cognitive API URL: {COGNITIVE_API_URL}")
     brain_config = load_brain_config(request.config)
     personality: Personality = brain_config.personality
@@ -66,7 +93,7 @@ Here is your personality profile:
         user_prompt_template = build_user_prompt_template(request.sensory, brain_config)
 
         logger.info(f"Original user prompt template: {user_prompt_template}")
-        user_prompt_template = await rewrite_user_prompt(user_prompt_template)
+        user_prompt_template = await thalamus(user_prompt_template)
         logger.info(f"Rewritten user prompt template: {user_prompt_template}")
 
         # Prepare the chat message for the Cognitive API

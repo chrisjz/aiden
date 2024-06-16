43b1a3ac2a664a9233ef2789a53d302dd9431439
Chris Zaharia <chris.zaharia@gmail.com>
2024-06-16 12:49:05 +1000

    Use langchain in thalamus processor

--- aiden/app/brain/cognition/thalamus.py
+++ aiden/app/brain/cognition/thalamus.py
@@ -1,14 +1,11 @@
-from aiden import logger
-from aiden.app.brain.cognition import COGNITIVE_API_URL_CHAT
-from aiden.models.brain import BrainConfig
-from aiden.models.chat import ChatMessage, Message, Options
-
-
-import httpx
-
-
-import json
 import os
+
+from langchain_community.chat_models import ChatOllama
+from langchain_core.messages import AIMessage, HumanMessage, SystemMessage
+
+from aiden import logger
+from aiden.app.brain.cognition import COGNITIVE_API_URL_BASE
+from aiden.models.brain import BrainConfig
 
 
 async def process_thalamus(sensory_input: str, brain_config: BrainConfig) -> str:
@@ -23,41 +20,29 @@ async def process_thalamus(sensory_input: str, brain_config: BrainConfig) -> str
         str: The rewritten sensory prompt or the original if the request fails.
     """
     instruction = "\n".join(brain_config.regions.thalamus.instruction)
-    messages = [
-        Message(role="system", content=instruction),
-        Message(role="user", content=sensory_input),
-    ]
-    chat_message = ChatMessage(
+
+    messages = [SystemMessage(content=instruction), HumanMessage(content=sensory_input)]
+
+    llm = ChatOllama(
+        base_url=COGNITIVE_API_URL_BASE,
         model=os.environ.get("COGNITIVE_MODEL", "bakllava"),
-        messages=messages,
-        stream=False,
-        options=Options(
-            frequency_penalty=1.2,
-            penalize_newline=False,
-            presence_penalty=1.0,
-            repeat_last_n=32,
-            repeat_penalty=1.0,
-            temperature=0.7,
-            top_k=40,
-            top_p=0.9,
-        ),
+        timeout=30.0,
+        frequency_penalty=1.2,
+        penalize_newline=False,
+        presence_penalty=1.0,
+        repeat_last_n=32,
+        repeat_penalty=1.0,
+        temperature=0.7,
+        top_k=40,
+        top_p=0.9,
     )
 
-    logger.info(
-        f"Thalamus chat message: {json.dumps(chat_message.model_dump(), indent=2)}"
-    )
-    async with httpx.AsyncClient() as client:
-        response = await client.post(
-            COGNITIVE_API_URL_CHAT, json=chat_message.model_dump(), timeout=30.0
-        )
-        if response.status_code == 200:
-            integrated_sensory_input = (
-                response.json().get("message", {}).get("content", sensory_input)
-            )
-            logger.info(f"Restructured sensory input: {integrated_sensory_input}")
-            return integrated_sensory_input
-        else:
-            logger.error(
-                f"Failed restructuring sensory input with status: {response.status_code}"
-            )
-            return sensory_input
+    logger.info(f"Thalamus chat message: {messages}")
+
+    response: AIMessage = llm.invoke(messages)
+    try:
+        logger.info(f"Restructured sensory input: {response.content}")
+        return response.content
+    except Exception as exc:
+        logger.error(f"Failed restructuring sensory input with error: {exc}")
+        return sensory_input
--- tests/unit/app/brain/cognition/test_thalamus.py
+++ tests/unit/app/brain/cognition/test_thalamus.py
@@ -1,34 +1,29 @@
-from aiden.app.brain.cognition.thalamus import process_thalamus
-
-
-import httpx
 import pytest
 
+from langchain_core.messages import AIMessage
 
-from unittest.mock import AsyncMock
+from aiden.app.brain.cognition.thalamus import process_thalamus
 
 
 @pytest.mark.asyncio
 async def test_process_thalamus_rewrite(mocker, brain_config):
-    # Mock the response from the cognitive API
-    mock_response = {
-        "message": {"content": "Rewritten sensory input based on narrative structure."}
-    }
+    # Create a mock response for the ChatOllama
+    mock_response = AIMessage(
+        content="Rewritten sensory input based on narrative structure."
+    )
 
-    with httpx.Client() as client:
-        client.post = AsyncMock(
-            return_value=httpx.Response(status_code=200, json=mock_response)
-        )
+    # Mock ChatOllama class to return a predefined response
+    mock_ollama = mocker.patch(
+        "aiden.app.brain.cognition.thalamus.ChatOllama", autospec=True
+    )
+    instance = mock_ollama.return_value
+    instance.invoke.return_value = mock_response
 
-        mocker.patch(
-            "httpx.AsyncClient.post",
-            return_value=httpx.Response(status_code=200, json=mock_response),
-        )
+    # Simulate the thalamus function call
+    rewritten_input = await process_thalamus("Initial sensory data", brain_config)
 
-        # Simulate the thalamus function call
-        rewritten_input = await process_thalamus("Initial sensory data", brain_config)
+    # Assert the rewritten input is as expected
+    assert rewritten_input == "Rewritten sensory input based on narrative structure."
 
-        # Assert the rewritten input is as expected
-        assert (
-            rewritten_input == "Rewritten sensory input based on narrative structure."
-        )
+    # Check that the invoke method was called correctly
+    instance.invoke.assert_called_once()

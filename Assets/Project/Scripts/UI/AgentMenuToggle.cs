1737926c43aaa04f6e930a27b093091e825748b1
Chris Zaharia <chris.zaharia@gmail.com>
2024-12-29 16:11:41 +1100

    Simplify agent menu toggle script

--- Assets/Project/Scripts/UI/AgentMenuToggle.cs
+++ Assets/Project/Scripts/UI/AgentMenuToggle.cs
@@ -5,15 +5,10 @@ using CandyCoded.env;
 public class AgentMenuToggle : MonoBehaviour
 {
     public GameObject menuCanvas;
-    public AutoScrollManager agentAutoScrollManager;
     public GameObject firstPersonCanvas;
     public GameObject playerChatCanvas;
-    public GameObject mainMenuCanvas;
     public PlayerInput playerInput;
     public PlayerLook playerLook;
-    public GameObject buttonSubmitAISpeak;
-    public GameObject buttonSubmitAIVisual;
-    public GameObject buttonSubmitUserRecording;
     public bool enableMenu = true;
 
     private bool isMenuVisible = false;
@@ -42,22 +37,12 @@ public class AgentMenuToggle : MonoBehaviour
                 Cursor.lockState = CursorLockMode.None; // Unlock the cursor
                 Cursor.visible = true;
 
-                // Toggle button visibilites based on respective APIs availability
-                buttonSubmitAIVisual.SetActive(isVisionEnabled);
-                buttonSubmitAISpeak.SetActive(isVocalEnabled);
-                buttonSubmitUserRecording.SetActive(isAuditoryLanguageEnabled);
-
-                // Disable agent output auto scrolling
-                agentAutoScrollManager.autoScrollEnabled = false;
-
-                mainMenuCanvas.SetActive(false);
                 playerChatCanvas.SetActive(false);
             }
             else
             {
                 Cursor.lockState = CursorLockMode.Locked; // Lock the cursor
                 Cursor.visible = false;
-                agentAutoScrollManager.autoScrollEnabled = true;
             }
         }
     }

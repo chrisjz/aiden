c009621e625d14a82f3b131cea82455c2919acb3
Chris Zaharia <chris.zaharia@gmail.com>
2025-01-19 20:40:55 +1100

    Move interact obj controller to AIden namespace

--- Assets/Project/Scripts/Characters/AI/InteractObjectController.cs
+++ Assets/Project/Scripts/Characters/AI/InteractObjectController.cs
@@ -1,129 +1,132 @@
 using UnityEngine;
 using System.Collections.Generic;
 
-public class InteractObjectController : MonoBehaviour
+namespace AIden
 {
-
-    [Tooltip("Name of the interactable object (e.g., Pantry)")]
-    public string objectName;
-
-    [Tooltip("Interaction range between object and players/agents")]
-    public float interactionRange = 1.8f;
-
-    [Tooltip("Output debug logs to console")]
-    public bool debugMode = false;
-
-    [Tooltip("Mapping of animations to their labels and states")]
-    // TODO: These maps should be tied to MoveableObject objects e.g. for large objects
-    // with multiple sections, such as a long kitchen bench with many drawers
-    public List<AnimationStateMapping> animationStateMappings;
-
-    private Animator _anim;
-
-    private HashSet<GameObject> _detectedObjects = new HashSet<GameObject>();
-
-    private bool _showInteractMsg;
-
-    private GUIStyle _guiStyle;
-
-    private string _interactiveMsg;
-
-    void Start()
+    public class InteractObjectController : MonoBehaviour
     {
-        // Initialize the animator for interaction
-        _anim = GetComponent<Animator>();
-        _anim.enabled = false;
 
-        // Setup GUI style settings for user prompts
-        setupGui();
-    }
+        [Tooltip("Name of the interactable object (e.g., Pantry)")]
+        public string objectName;
 
-    void Update()
-    {
-        if (_detectedObjects.Count > 0)
+        [Tooltip("Interaction range between object and players/agents")]
+        public float interactionRange = 1.8f;
+
+        [Tooltip("Output debug logs to console")]
+        public bool debugMode = false;
+
+        [Tooltip("Mapping of animations to their labels and states")]
+        // TODO: These maps should be tied to MoveableObject objects e.g. for large objects
+        // with multiple sections, such as a long kitchen bench with many drawers
+        public List<AnimationStateMapping> animationStateMappings;
+
+        private Animator _anim;
+
+        private HashSet<GameObject> _detectedObjects = new HashSet<GameObject>();
+
+        private bool _showInteractMsg;
+
+        private GUIStyle _guiStyle;
+
+        private string _interactiveMsg;
+
+        void Start()
         {
-            _interactiveMsg = $"{objectName} - Actions:\n";
-            for (int i = 0; i < Mathf.Min(animationStateMappings.Count, 9); i++)
+            // Initialize the animator for interaction
+            _anim = GetComponent<Animator>();
+            _anim.enabled = false;
+
+            // Setup GUI style settings for user prompts
+            setupGui();
+        }
+
+        void Update()
+        {
+            if (_detectedObjects.Count > 0)
             {
-                var mapping = animationStateMappings[i];
-                bool currentState = _anim.GetBool(mapping.animBoolName);
-                string stateLabel = currentState ? mapping.offLabel : mapping.onLabel;
-
-                _interactiveMsg += $"Press {i + 1} to {stateLabel}\n";
-
-                if (Input.GetKeyDown(KeyCode.Alpha1 + i))
+                _interactiveMsg = $"{objectName} - Actions:\n";
+                for (int i = 0; i < Mathf.Min(animationStateMappings.Count, 9); i++)
                 {
-                    PerformAction(mapping);
+                    var mapping = animationStateMappings[i];
+                    bool currentState = _anim.GetBool(mapping.animBoolName);
+                    string stateLabel = currentState ? mapping.offLabel : mapping.onLabel;
+
+                    _interactiveMsg += $"Press {i + 1} to {stateLabel}\n";
+
+                    if (Input.GetKeyDown(KeyCode.Alpha1 + i))
+                    {
+                        PerformAction(mapping);
+                    }
+                }
+            }
+            else
+            {
+                _interactiveMsg = "";
+            }
+        }
+
+        private void PerformAction(AnimationStateMapping mapping)
+        {
+            bool currentState = _anim.GetBool(mapping.animBoolName);
+            _anim.enabled = true;
+            _anim.SetBool(mapping.animBoolName, !currentState);
+
+            if (debugMode) Debug.Log($"Performed action: {(currentState ? mapping.offLabel : mapping.onLabel)} on {objectName}");
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.GetComponent<CharacterController>() != null)
+            {
+                _detectedObjects.Add(other.gameObject);
+
+                if (other.CompareTag("Player"))
+                {
+                    _showInteractMsg = true;
                 }
             }
         }
-        else
+
+        private void OnTriggerExit(Collider other)
         {
+            if (_detectedObjects.Contains(other.gameObject))
+            {
+                _detectedObjects.Remove(other.gameObject);
+
+                if (other.CompareTag("Player"))
+                {
+                    _showInteractMsg = false;
+                }
+            }
+        }
+
+        #region GUI Config
+
+        private void setupGui()
+        {
+            _guiStyle = new GUIStyle();
+            _guiStyle.fontSize = 16;
+            _guiStyle.fontStyle = FontStyle.Bold;
+            _guiStyle.normal.textColor = Color.white;
             _interactiveMsg = "";
         }
-    }
 
-    private void PerformAction(AnimationStateMapping mapping)
-    {
-        bool currentState = _anim.GetBool(mapping.animBoolName);
-        _anim.enabled = true;
-        _anim.SetBool(mapping.animBoolName, !currentState);
-
-        if (debugMode) Debug.Log($"Performed action: {(currentState ? mapping.offLabel : mapping.onLabel)} on {objectName}");
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.GetComponent<CharacterController>() != null)
+        void OnGUI()
         {
-            _detectedObjects.Add(other.gameObject);
-
-            if (other.CompareTag("Player"))
+            if (_showInteractMsg)
             {
-                _showInteractMsg = true;
+                GUI.Label(new Rect(50, 100, 400, 100), _interactiveMsg, _guiStyle);
             }
         }
-    }
 
-    private void OnTriggerExit(Collider other)
-    {
-        if (_detectedObjects.Contains(other.gameObject))
+        #endregion
+
+        [System.Serializable]
+        public class AnimationStateMapping
         {
-            _detectedObjects.Remove(other.gameObject);
-
-            if (other.CompareTag("Player"))
-            {
-                _showInteractMsg = false;
-            }
+            public string animBoolName;
+            public string onLabel;
+            public string offLabel;
         }
-    }
-
-    #region GUI Config
-
-    private void setupGui()
-    {
-        _guiStyle = new GUIStyle();
-        _guiStyle.fontSize = 16;
-        _guiStyle.fontStyle = FontStyle.Bold;
-        _guiStyle.normal.textColor = Color.white;
-        _interactiveMsg = "";
-    }
-
-    void OnGUI()
-    {
-        if (_showInteractMsg)
-        {
-            GUI.Label(new Rect(50, 100, 400, 100), _interactiveMsg, _guiStyle);
-        }
-    }
-
-    #endregion
-
-    [System.Serializable]
-    public class AnimationStateMapping
-    {
-        public string animBoolName;
-        public string onLabel;
-        public string offLabel;
     }
 }

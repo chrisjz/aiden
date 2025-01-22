d1fa14dc4be55b2327763ad5b05fec344ce9615a
Chris Zaharia <chris.zaharia@gmail.com>
2025-01-22 23:02:40 +1100

    feat(unity): Add agent interactions with objects in environment (#14)
    * Add script for interactive objects
    
    * Add TODO
    
    * Add GUI to interactive objects
    
    * Generic handling of object actions
    
    * Add TODO
    
    * Add interact controller to objects in scene
    
    * Change position of interaction message

---
+++ Assets/Project/Scripts/Characters/AI/InteractObjectController.cs
@@ -0,0 +1,129 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class InteractObjectController : MonoBehaviour
+{
+
+    [Tooltip("Name of the interactable object (e.g., Pantry)")]
+    public string objectName;
+
+    [Tooltip("Interaction range between object and players/agents")]
+    public float interactionRange = 1.8f;
+
+    [Tooltip("Output debug logs to console")]
+    public bool debugMode = false;
+
+    [Tooltip("Mapping of animations to their labels and states")]
+    // TODO: These maps should be tied to MoveableObject objects e.g. for large objects
+    // with multiple sections, such as a long kitchen bench with many drawers
+    public List<AnimationStateMapping> animationStateMappings;
+
+    private Animator _anim;
+
+    private HashSet<GameObject> _detectedObjects = new HashSet<GameObject>();
+
+    private bool _showInteractMsg;
+
+    private GUIStyle _guiStyle;
+
+    private string _interactiveMsg;
+
+    void Start()
+    {
+        // Initialize the animator for interaction
+        _anim = GetComponent<Animator>();
+        _anim.enabled = false;
+
+        // Setup GUI style settings for user prompts
+        setupGui();
+    }
+
+    void Update()
+    {
+        if (_detectedObjects.Count > 0)
+        {
+            _interactiveMsg = $"{objectName} - Actions:\n";
+            for (int i = 0; i < Mathf.Min(animationStateMappings.Count, 9); i++)
+            {
+                var mapping = animationStateMappings[i];
+                bool currentState = _anim.GetBool(mapping.animBoolName);
+                string stateLabel = currentState ? mapping.offLabel : mapping.onLabel;
+
+                _interactiveMsg += $"Press {i + 1} to {stateLabel}\n";
+
+                if (Input.GetKeyDown(KeyCode.Alpha1 + i))
+                {
+                    PerformAction(mapping);
+                }
+            }
+        }
+        else
+        {
+            _interactiveMsg = "";
+        }
+    }
+
+    private void PerformAction(AnimationStateMapping mapping)
+    {
+        bool currentState = _anim.GetBool(mapping.animBoolName);
+        _anim.enabled = true;
+        _anim.SetBool(mapping.animBoolName, !currentState);
+
+        if (debugMode) Debug.Log($"Performed action: {(currentState ? mapping.offLabel : mapping.onLabel)} on {objectName}");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<CharacterController>() != null)
+        {
+            _detectedObjects.Add(other.gameObject);
+
+            if (other.CompareTag("Player"))
+            {
+                _showInteractMsg = true;
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (_detectedObjects.Contains(other.gameObject))
+        {
+            _detectedObjects.Remove(other.gameObject);
+
+            if (other.CompareTag("Player"))
+            {
+                _showInteractMsg = false;
+            }
+        }
+    }
+
+    #region GUI Config
+
+    private void setupGui()
+    {
+        _guiStyle = new GUIStyle();
+        _guiStyle.fontSize = 16;
+        _guiStyle.fontStyle = FontStyle.Bold;
+        _guiStyle.normal.textColor = Color.white;
+        _interactiveMsg = "";
+    }
+
+    void OnGUI()
+    {
+        if (_showInteractMsg)
+        {
+            GUI.Label(new Rect(50, 100, 400, 100), _interactiveMsg, _guiStyle);
+        }
+    }
+
+    #endregion
+
+    [System.Serializable]
+    public class AnimationStateMapping
+    {
+        public string animBoolName;
+        public string onLabel;
+        public string offLabel;
+    }
+}
